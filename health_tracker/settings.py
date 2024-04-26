812db4d869212732c5498fa3331554b09181b9c0
umerfarok <umerfarooq.dev@gmail.com>
2024-04-26 21:56:50 +0500

    Update DEBUG setting to default to True in settings.py Add index view to urls.py

--- health_tracker/settings.py
+++ health_tracker/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY', default='django-insecure$@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG' , cast=bool, default=True)
 
 ALLOWED_HOSTS = []
 
--- health_tracker/urls.py
+++ health_tracker/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
@@ -11,4 +11,5 @@ urlpatterns = [
     path('reminders/', include('reminders.urls', namespace='reminders')),
     path('community/', include('community.urls', namespace='community')),
     path('', include('django.contrib.auth.urls')),
+    path('', views.index, name='index'),
 ]
